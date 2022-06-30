a443230fe4eec69708c4d173b7ef16130b7157bc
ErikHartman <erik.hartman@hotmail.com>
2022-06-30 10:20:56 +0200

    Shapley values and ML

--- src/useful_functions.py
+++ src/useful_functions.py
@@ -1,6 +1,7 @@
 import math
 from scipy import stats
 import numpy as np
+import pandas as pd
 
 def remove_nan(array):
     return [x for x in array if not(math.isnan(x)) == True]
@@ -22,3 +23,10 @@ def get_difference(row, GroupOneCols, GroupTwoCols):
     val_two = remove_nan(row[GroupTwoCols].values)
     
     return np.mean(val_one)- np.mean(val_two)
+
+def add_protein_trivnames(my_df, protein_col_name):
+    protein_df = pd.read_csv('data/human_proteome.gz')
+    protein_df = protein_df[['accession','trivname']]
+    my_df = my_df.merge(protein_df, left_on=protein_col_name, right_on='accession', how='left')
+    return my_df
+
