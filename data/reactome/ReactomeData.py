13287121d54f26b14432706bca494742c73d80f3
ErikHartman <erik.hartman@hotmail.com>
2022-07-04 10:57:29 +0200

    class for reactome data

--- data/reactome/ReactomeData.py
+++ data/reactome/ReactomeData.py
@@ -3,18 +3,18 @@ import pandas as pd
 class ReactomeData():
     """ A class to subset and get reactome data.
     
-    file1 = 'ReactomePathwaysRelation.txt'
-    file2 = '../ms/....' - this should be a file with column "Proteins"
-    file3 = 'UniProt2Reactome_All_Levels.txt"
+    file1 = 'data/reactome/ReactomePathwaysRelation.txt'
+    file2 = 'data/ms/proteins.csv' 
+    file3 = "data/reactome/UniProt2Reactome_All_Levels.txt"
     RD = ReactomeData(file1, file2, file3)
     RD.subset_species_reactome_data('Homo sapiens')
-    RD.subset_on_protein_in_ms_data()
-    RD.subset_hierarchies_on_file()
+    RD.subset_on_proteins_in_ms_data()
+    RD.subset_pathways_on_reactome_idx()
     RD.save_df("reactome", "HSA_ms_reactome.csv") 
-    RD.save_df("path", 'HSA_ms_path.csv") 
+    RD.save_df("path", "HSA_ms_path.csv")
     
     The two saved files are the reactome and path files for the reactome which can be used to
-    create the neural network. 
+    create the network. 
     """
     
     def __init__(self,hierarchy_file_path, ms_data_file_path, reactome_all_data_file_path):
@@ -24,36 +24,60 @@ class ReactomeData():
         self.ms_df = pd.read_csv(ms_data_file_path)
         self.reactome_df = pd.read_csv(self.reactome_all_data_file_path, index_col=False,
                         names = ['UniProt_id', 'Reactome_id', 'URL', 'Description','Evidence Code','Species'], sep="\t")
-        self.path_df = pd.read_csv(hierarchy_file_path, names=['From','To'], index_col=False)
+        self.path_df = pd.read_csv(hierarchy_file_path, names=['parent','child'], sep="\t", index_col=False)
         
-    def subset_species_sapiens_reactome_data(self, species = 'Homo sapiens'):
+    def subset_species_reactome_data(self, species = 'Homo sapiens'):
         df_species = self.reactome_df[self.reactome_df['Species'] == species]
         print(f"Number of rows of {species}: {len(df_species.index)}")
+
         self.reactome_df = df_species
         return self.reactome_df
         
-    def subset_on_protein_in_ms_data(self):
+    def subset_on_proteins_in_ms_data(self):
         proteins_in_ms_data = self.ms_df['Proteins'].values
-        self.reactome_df = self.reactome_df[self.df['Proteins'].isin(proteins_in_ms_data)]
-        return self.df
+        print(f'Number of reactome ids before subsetting: {len(self.reactome_df.index)}')
+        self.reactome_df = self.reactome_df[self.reactome_df['UniProt_id'].isin(proteins_in_ms_data)]
+        print(f"Number of reactome ids after subsettign: {len(self.reactome_df.index)}")
+        print(f"Unique proteins in reactome df: {len(list(self.reactome_df['UniProt_id'].unique()))}")
+        return self.reactome_df
         
-    def subset_hierarchies_on_file(self):
-        reactome_ids = self.reactome_df['Reactome_id'].values
+    def subset_pathways_on_reactome_idx(self):
+        """
+        We want this function to use the reactome idx to subset the pathway-file.
+        The first step is just to find in which "parent" the reactom idx exist.
+        Thereafter we want to find which "To" exist in "parent" and iterate this step.
+        This sounds like recursion to me!
         
-        def keep_row(row):
-            # Should both From and To contain the protein or just From
-            protein1 =row['From'] 
-            protein2 = row['To']
-            if protein1 not in reactome_ids:
-                return False
-            if protein2 not in reactome_ids:
-                return False
-            return True
+        Want a function which takes list and "parent" as input and adds the parent to list.
+        Thereafter the function will be called with the list and "Child". 
         
-        subsetted['in_subset'] = self.path_df.apply(lambda x: keep_row(x), axis=1)
-        subsetted = subsetted[subsetted['in_subset'] == True]
-        subsetted.drop(['in_subset'], inplace=True)
-        self.path_df = subsetted
+        Base case: Child is empty. Return List.
+        """
+        def add_pathways(counter, reactome_idx_list, parent):
+            counter += 1
+            print(f"Function called {counter} times.")
+            print(f'Values in reactome_idx_list: {len(reactome_idx_list)}')
+            if len(parent) == 0:
+                print('Base case reached')
+                return reactome_idx_list
+            else:
+                reactome_idx_list = reactome_idx_list + parent
+                subsetted_pathway = self.path_df[self.path_df['parent'].isin(parent)]
+                new_parent = list(subsetted_pathway['child'].unique())
+                print(f"Values in new_parent: {len(new_parent)}")
+                return add_pathways(counter, reactome_idx_list, new_parent)
+                
+        counter = 0    
+        original_parent = list(self.reactome_df['Reactome_id'].unique())
+        reactome_idx_list = []
+        reactome_idx_list = add_pathways(counter, reactome_idx_list, original_parent)
+        print(f"Final number of values in reactome list: {len(reactome_idx_list)}")
+        # the reactome_idx_list now contains all reactome idx which we want to subset the path_df on.
+        self.path_df = self.path_df[self.path_df['parent'].isin(reactome_idx_list)]
+        print("Final number of unique connections in pathway: ", len(self.path_df.index))
+        return self.path_df
+ 
+ 
  
     def save_df(self, df_id, save_path):
         if df_id == 'reactome':
@@ -68,3 +92,20 @@ if __name__ == '__main__':
     print('|S C I E N C E|')
     print('--------------')
     print('\( o _ o )/')
+    
+    
+    file1 = 'data/reactome/ReactomePathwaysRelation.txt'
+    file2 = 'data/ms/proteins.csv' 
+    file3 = "data/reactome/UniProt2Reactome_All_Levels.txt"
+    RD = ReactomeData(file1, file2, file3)
+    RD.subset_species_reactome_data('Homo sapiens')
+    RD.subset_on_proteins_in_ms_data()
+    RD.subset_pathways_on_reactome_idx()
+    RD.save_df("reactome", "data/reactome/HSA_ms_reactome.csv") 
+    RD.save_df("path", "data/reactome/HSA_ms_path.csv")
+    
+    
+    
+    
+    
+    
